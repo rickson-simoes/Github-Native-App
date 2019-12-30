7b51c5ec10279b65a4af624798c20dc81ff4787a
Rickson <rickson.simoes@hotmail.com>
2019-12-30 14:33:34 -0300

    :wrench: EsLint/ Prettier - Configurados

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}> Hello World </Text>
-      </View>
-    );
-  }
-}
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F1F1F1',
+    backgroundColor: 'black'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
+    color: 'white'
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}> Hello World - Black </Text>
+    </View>
+  );
+}
