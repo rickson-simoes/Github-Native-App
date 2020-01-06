5eda9a3b3870e01dfb001f610d124aa13e1df6a2
Rickson <rickson.simoes@hotmail.com>
2020-01-06 18:13:22 -0300

    :package: - React-native-vector-icons

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 20px;
   background: #eee;
 `;
+
+export const Form = styled.View``;
+
+export const Input = styled.TextInput``;
+
+export const SubmitButton = styled(RectButton)``;
