009a6fa213689daefafe4d02041fd390ab5f044f
Rickson <rickson.simoes@hotmail.com>
2020-01-07 17:29:00 -0300

    :hammer: Adicionando componente State - Inserindo dados dentro do state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: []
+  };
 
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleAddUser = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
