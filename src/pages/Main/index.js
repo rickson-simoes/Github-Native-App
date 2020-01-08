87a5203a0ef64a8aecf38457960f9334400230c5
Rickson <rickson.simoes@hotmail.com>
2020-01-08 16:54:40 -0300

    :hammer: ao adicionar um usuario, agora um loading é executado

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
@@ -20,11 +20,14 @@ import {
 export default class Main extends Component {
   state = {
     newUser: '',
-    users: []
+    users: [],
+    loading: false
   };
 
   handleAddUser = async () => {
     const { newUser, users } = this.state;
+
+    this.setState({ loading: true });
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -36,14 +39,15 @@ export default class Main extends Component {
     };
     this.setState({
       users: [...users, data],
-      newUser: ''
+      newUser: '',
+      loading: false
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -58,8 +62,12 @@ export default class Main extends Component {
             onSubmitEditing={this.handleAddUser}
           />
 
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#FFF" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#FFF" />
+            )}
           </SubmitButton>
         </Form>
 
