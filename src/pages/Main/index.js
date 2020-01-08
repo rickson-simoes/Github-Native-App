bbe9d7b667757b8dcb8495a46b5ed3cf6f5a4ddc
Rickson <rickson.simoes@hotmail.com>
2020-01-08 12:23:56 -0300

    :package: Axios adicionado / :wrench: Consultando api do github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
@@ -10,8 +11,23 @@ export default class Main extends Component {
     users: []
   };
 
-  handleAddUser = () => {
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const { newUser, users } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url
+    };
+    this.setState({
+      users: [...users, data],
+      newUser: ''
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
