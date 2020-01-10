16a0bef35d05dd02120eb025b03380e5fb9f96b4
Rickson <rickson.simoes@hotmail.com>
2020-01-10 10:39:51 -0300

    :rewind: removendo config de host do reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -4,7 +4,7 @@ import Reactotron from 'reactotron-react-native';
 // 192.168.0.10
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '10.51.172.25' })
+  const tron = Reactotron.configure()
     .useReactNative()
     .connect();
 
